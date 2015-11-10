d0930c393ad009e9481477da4b4aebf1609bdf5b
borjargz <borja.rodriguez1@udc.es>
2015-11-10 10:38:12 +0100

    Eliminando test anteriores

--- servidor/src/test/java/contenido/AnuncioTest.java
+++ servidor/src/test/java/contenido/AnuncioTest.java
@@ -1,102 +1,41 @@
-/**
- * 
- */
 package contenido;
 
-import static org.junit.Assert.*;
+import junit.framework.TestCase;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+public class AnuncioTest extends TestCase {
 
-/**
- * @author borja
- *
- */
-public class AnuncioTest {
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	protected void setUp() throws Exception {
+		super.setUp();
 	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
+	protected void tearDown() throws Exception {
+		super.tearDown();
 	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	/**
-	 * Test method for {@link contenido.AnuncioTest#Anuncio()}.
-	 */
-	@Test
 	public void testAnuncio() {
 		fail("Not yet implemented");
 	}
 
-	/**
-	 * Test method for {@link contenido.AnuncioTest#obtenerTitulo()}.
-	 */
-	@Test
 	public void testObtenerTitulo() {
 		fail("Not yet implemented");
 	}
 
-	/**
-	 * Test method for {@link contenido.AnuncioTest#obtenerDuracion()}.
-	 */
-	@Test
 	public void testObtenerDuracion() {
 		fail("Not yet implemented");
 	}
 
-	/**
-	 * Test method for {@link contenido.AnuncioTest#obtenerListaReproduccion()}.
-	 */
-	@Test
 	public void testObtenerListaReproduccion() {
 		fail("Not yet implemented");
 	}
 
-	/**
-	 * Test method for {@link contenido.AnuncioTest#buscar(java.lang.String)}.
-	 */
-	@Test
 	public void testBuscar() {
 		fail("Not yet implemented");
 	}
 
-	/**
-	 * Test method for {@link contenido.AnuncioTest#agregar(contenido.Contenido, contenido.Contenido)}.
-	 */
-	@Test
 	public void testAgregar() {
 		fail("Not yet implemented");
 	}
 
-	/**
-	 * Test method for {@link contenido.AnuncioTest#eliminar(contenido.Contenido)}.
-	 */
-	@Test
 	public void testEliminar() {
 		fail("Not yet implemented");
 	}
--- servidor/src/test/java/contenido/EmisoraTest.java
+++ servidor/src/test/java/contenido/EmisoraTest.java
@@ -1,57 +1,37 @@
 package contenido;
 
-import static org.junit.Assert.*;
+import junit.framework.TestCase;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+public class EmisoraTest extends TestCase {
 
-public class EmisoraTest {
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	protected void setUp() throws Exception {
+		super.setUp();
 	}
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
+	protected void tearDown() throws Exception {
+		super.tearDown();
 	}
 
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	@Test
 	public void testObtenerTitulo() {
 		fail("Not yet implemented");
 	}
 
-	@Test
 	public void testObtenerDuracion() {
 		fail("Not yet implemented");
 	}
 
-	@Test
 	public void testObtenerListaReproduccion() {
 		fail("Not yet implemented");
 	}
 
-	@Test
 	public void testBuscar() {
 		fail("Not yet implemented");
 	}
 
-	@Test
 	public void testAgregar() {
 		fail("Not yet implemented");
 	}
 
-	@Test
 	public void testEliminar() {
 		fail("Not yet implemented");
 	}
