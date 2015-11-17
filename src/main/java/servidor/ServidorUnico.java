0bf4703f6f256eb7d50c18476cc5b122e932b567
borjargz <borja.rodriguez1@udc.es>
2015-11-17 17:33:56 +0100

    Eliminado codigo superfluo en buscar, #issue12.

--- src/main/java/servidor/ServidorUnico.java
+++ src/main/java/servidor/ServidorUnico.java
@@ -1,19 +1,9 @@
 package servidor;
 
-import java.util.List;
-
-import contenido.Contenido;
-
 public class ServidorUnico extends ServidorGenerico{
 	
 	public ServidorUnico(String nombre,String adminToken) {
 		super(nombre,adminToken);
 	}
-
-	public List<Contenido> Buscar(String buscar, String tk){
-		return super.Buscar(buscar, tk);
-	}
-
-
 
 }
